43859bea484f443b65290686aebf54b210b3165c
saivigneshmuthyam1 <saivigneshmuthyam1@gmail.com>
2026-05-29 13:51:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-# Main Streamlit entry point
-from pages.dashboard import run_dashboard
-run_dashboard()
+import streamlit as st
+import pandas as pd
+import joblib
+
+st.set_page_config(
+    page_title="Employee Attrition Predictor",
+    layout="wide"
+)
+
+model = joblib.load("attrition_model_v2.pkl")
+
+st.title("AI Powered Employee Attrition Prediction System")
+
+st.write("Predict employee attrition risk using Machine Learning")
